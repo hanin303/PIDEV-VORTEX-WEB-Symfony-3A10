111490d56a1aca0655b6d940467e90943592e17b
abirmachraoui <abir.machraoui@hotmail.com>
2023-04-13 04:27:37 +0100

    abir_login

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,9 +6,18 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User
+#[UniqueEntity('email',"Adresse e-mail existe déjà")]
+#[UniqueEntity('username',"Username existe déjà")]
+#[UniqueEntity('CIN',"Carte identité existe dèjà")]
+
+
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -21,19 +30,19 @@ class User
     #[ORM\Column(length: 255)]
     private ?string $prenom = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255,unique :true)]
     private ?string $username = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255,unique :true)]
     private ?string $email = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $mdp = null;
+    #[ORM\Column(name: 'mdp',length: 255)]
+    private ?string $password = null;
 
     #[ORM\Column]
     private ?int $num_tel = null;
 
-    #[ORM\Column]
+    #[ORM\Column(unique :true)]
     private ?int $CIN = null;
 
     #[ORM\Column(length: 255 , nullable: true)]
@@ -52,6 +61,8 @@ class User
     #[ORM\ManyToOne(inversedBy: 'id_user')]
     #[ORM\JoinColumn(nullable: true)]
     private ?UserState $id_state = null;
+
+    private ?string $token ;
 
     public function __construct()
     {
@@ -112,14 +123,14 @@ class User
         return $this;
     }
 
-    public function getMdp(): ?string
+    public function getPassword(): ?string
     {
-        return $this->mdp;
+        return $this->password;
     }
 
-    public function setMdp(string $mdp): self
+    public function setPassword(string $password): self
     {
-        $this->mdp = $mdp;
+        $this->password = $password;
 
         return $this;
     }
@@ -170,6 +181,25 @@ class User
         $this->id_role = $id_role;
 
         return $this;
+    }
+    public function getToken(): ?string
+    {
+        return $this->token;
+    }
+
+    public function setToken(?string $token): self
+    {
+        $this->token = $token;
+
+        return $this;
+    }
+    public function state(User $user):bool{
+        if($user->getIdState()==1){
+            return false;
+        }else{
+            return true;
+        }
+
     }
 
     /**
@@ -242,5 +272,51 @@ class User
         $this->id_state = $id_state;
 
         return $this;
+    }
+    /**
+     * The public representation of the user (e.g. a username, an email address, etc.)
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->username;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): void
+    {
+        $roles = $this->id_role;
+        // guarantee every user at least has ROLE_USER
+    }
+
+    public function setRoles(Role $roles): self
+    {
+        $this->id_role = $roles;
+
+        return $this;
+    }
+
+
+    /**
+     * Returning a salt is only needed if you are not using a modern
+     * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
+     *
+     * @see UserInterface
+     */
+    public function getSalt(): ?string
+    {
+        return null;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 }
