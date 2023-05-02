501c3a50bdb354d407950938c4b332dea327b5f1
wejdeneh <wejdene.hadjmefteh@esprit.tn>
2023-05-02 19:40:19 +0100

    wejdeene_API_Metiers

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\MoyenTransport;
+use App\Repository\MoyenTransportRepository;
+use App\Form\MoyenTransportRatingType;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Doctrine\Persistence\ManagerRegistry;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+
+
 
 class HomeController extends AbstractController
 {
@@ -22,12 +35,81 @@ class HomeController extends AbstractController
         return $this->render('ticket/tarif.html.twig');
     }
 
-    #[Route('/lignes', name: 'lignes_urbaine')]
-    public function listLignes(): Response
-    {
-        return $this->render('moyentransport/ligne.html.twig');
-    }
+     #[Route('/lignes', name: 'lignes_urbaine' , methods: ['GET'])]
+    public function listLignes(MoyenTransportRepository $moyenTransportRepository): Response
+     {
+         return $this->render('moyen_transport/FrontIndex.html.twig', [
+            'moyen_transports' => $moyenTransportRepository->findAll(),
+        ]);
+     }
+ 
 
+     #[Route('/star/{id}', name: 'star')]
+     public function yourAction(Request $request, $id, ManagerRegistry $doctrine)
+ {
+     if ($request->isXmlHttpRequest()) {
+         // handle the AJAX request
+         $data = $request->getContent(); // retrieve the data sent by the client-side JavaScript code
+         $repository = $doctrine->getRepository(MoyenTransport::class);
+         $moys = $repository->find($id);
+         
+         if ($moys->getNote() == 0) {
+             $moys->setNote($data[6]);
+         } else {
+             $newNote = ($moys->getNote() + $data[6]) / 2;
+             if ($newNote < $moys->getNote()) {
+                 $newNote = $moys->getNote();
+             }
+             $moys->setNote($newNote);
+         }
+         
+         $em=$doctrine->getManager();
+         $em->persist($moys);
+         $em->flush();
+         $bl = $repository->find($id);
+         $test=$bl->getNote();
+         $response = new Response();//nouvelle instance du response pour la renvoyer a la fonction ajax
+         $response->setContent(json_encode($test));//encoder les donnes sous forme JSON et les attribuer a la variable response
+         $response->headers->set('Content-Type', 'application/json');
+         return $response;//envoie du response
+     } 
+ }
+
+   /*   #[Route('/lignes/{id}/rate', name: 'moyen_transport_rate' )]
+     public function rate(Request $request, MoyenTransport $moyenTransport)
+     {
+       $form = $this->createForm(MoyenTransportRatingType::class,  $moyenTransport);
+
+       $form->handleRequest($request);
+   
+       if ($form->isSubmitted() && $form->isValid()) {
+           $rating = $form->get('note')->getData();
+   
+           $moyenTransport->setNote(floatval($rating));
+   
+           $entityManager = $this->getDoctrine()->getManager();
+           $entityManager->persist($moyenTransport);
+           $entityManager->flush();
+   
+           return $this->redirectToRoute('lignes_urbaine', ['id' => $moyenTransport->getId()]);
+       }
+   
+       return $this->render('moyen_transport/rate.html.twig', [
+           'moyen_transport' => $moyenTransport,
+           'form' => $form->createView(),
+       ]);
+     } */
+
+     #[Route('/lignes/{id}', name: 'lignes_show' , methods: ['GET'])]
+     public function ligne(MoyenTransport $moyenTransport): Response
+      {
+          return $this->render('moyen_transport/FrontShow.html.twig', [
+             'moyen_transport' => $moyenTransport,
+         ]);
+      }
+
+      
+  
     #[Route('/itineraires', name: 'voyager_itineraire')]
     public function listItineraires(): Response
     {
@@ -45,4 +127,6 @@ class HomeController extends AbstractController
     {
         return $this->render('reclamation/reclamation.html.twig');
     }
+
+    
 }
